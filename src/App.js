1260f0876c757c37ac102ef5ccce5b4dcbfe8846
MaakuW <maakuwemail@gmail.com>
2022-12-22 11:15:27 -0500

    Merged the two designs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo_stacked from './logo--stacked.svg'
 import logo from './logo--gold.svg'
 import entryway from './7685682602157855425.jpg'
 import redWalls from './1662784570334604561.jpg'
+import office from './office-building-construction-site-with-room-for-text-1570x1047.jpg'
 import redBuilding from './5315839180785235096.jpg'
 import couches from './7729529688808813175.jpg'
 import skyscraper from './office-building-skyscrapers-with-room-for-text-picjumbo-com.jpg'
@@ -9,9 +10,8 @@ import skyscraper from './office-building-skyscrapers-with-room-for-text-picjumb
 function App() {
   const rowStyle    = "flex flex-wrap h-auto w-full"
   const conStyle    = "py-8 md:py-16 px-4 md:px-12 xl:px-24"
-  const fDdStyle    = "order-2 text-gold"
-  const fDtStyle    = "order-1 font-semibold mb-1 md:mb-2"
-  const fLinkStyle  = "py-1 md:py-2 pr-3 uppercase text-white font-bold"
+  const fDdStyle    = "md:order-2 mb-4 md:mb-0 text-gold"
+  const fDtStyle    = "md:order-1 font-semibold mb-1 md:mb-2"
   const naStyle     = "p-1 md:p-2 text-sm"
   const slideStyle  = "p-2 md:p-4 mb-4 md:mb-auto cursor-pointer"
 
@@ -27,7 +27,7 @@ function App() {
     return <h2 className="text-xl md:text-2xl lg:text-4xl font-extrabold text-white">{copy}</h2>
   }
 
-  const makeSlide = (img,h,c0="#542825",c1="rgba(27, 40, 55, 0.84)",c2="hsla(8, 27%, 59%, 1)",alt="") => {
+  const makeSlide = (img,h,c0="#542825",c1="rgba(27, 40, 55, 0.84)",c2="hsla(8, 27%, 59%, 1)",alt="",width="",height="") => {
     return (
       <>
         <figure className="relative h-full w-full" style={{
@@ -44,7 +44,7 @@ function App() {
           event.target.style.backgroundPosition = 'center, center'
           event.target.style.backgroundRepeat = 'no-repeat, no-repeat'
         }}>
-          <img alt={alt} src={img} className="w-full h-auto invisible"/>
+          <img alt={alt} src={img} width={width} height={height} className="invisible"/>
           <figcaption className="pointer-events-none absolute top-0 left-0 w-full p-4 md:p-8 lg:p-16 text-center">
             <h4 className="w-full w-1/2 md:mb-2 mx-auto text-lg font-semibold" style={{color:c2}}>{h}</h4>
             <hr className="w-20 mb-12" style={{backgroundColor:c2}}/>
@@ -72,7 +72,7 @@ function App() {
     return (
       <>
         <li className="flex items-center justify-start py-4 md:py-8">
-          <div className="text-6xl md:text-8xl">
+          <div className="text-5xl sm:text-6xl md:text-8xl">
             <svg style={{width:'1em',height:'1em'}} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
               <rect x="0.458748" y="-0.10791" width="31.9587" height="32.6689" fill={color1}/>
               <path d="M8.24427 25.1752L25.406 25.1752" stroke={color2} strokeLinecap="round"/>
@@ -80,7 +80,7 @@ function App() {
               <path d="M8.43578 7.52007C8.16069 7.52007 7.93768 7.74478 7.93768 8.02197L7.93768 12.5391C7.93768 12.8163 8.16069 13.041 8.43578 13.041C8.71087 13.041 8.93387 12.8163 8.93387 12.5391L8.93387 8.52387L12.9186 8.52387C13.1937 8.52387 13.4167 8.29916 13.4167 8.02197C13.4167 7.74478 13.1937 7.52007 12.9186 7.52007L8.43578 7.52007ZM25.091 25.5142C25.2855 25.7102 25.6009 25.7102 25.7954 25.5142C25.9899 25.3182 25.9899 25.0004 25.7954 24.8044L25.091 25.5142ZM8.08357 8.37687L25.091 25.5142L25.7954 24.8044L8.78798 7.66708L8.08357 8.37687Z" fill={color2}/>
             </svg>
           </div>
-          <span className="block ml-4 mr-12">{title}</span>
+          <span className="block ml-4 md:mr-12">{title}</span>
         </li>
       </>
     )
@@ -89,8 +89,8 @@ function App() {
   const makeTout = (title, description) => {
     return (
       <>
-        <dt data-tout className="text-chocolate-300 md:order-1 mt-8 md:mt-auto mb-2 md:mb-4 font-semibold">{title}</dt>
-        <dd className="text-neutral-300 md:order-2 text-sm md:text-base">
+        <dt data-tout className="md:order-1 mt-8 md:mt-auto mb-2 md:mb-4 font-semibold">{title}</dt>
+        <dd className="md:order-2 text-sm md:text-base">
           <p>{description}</p>
         </dd>
       </>
@@ -113,14 +113,15 @@ function App() {
     <main className="md:min-h-screen bg-neutral-850">
       <header className="absolute top-0 left-0 z-50 w-full flex nowrap items-center justify-start px-4 sm:px-8 py-2 md:py-4 bg-black-1/3">
         <figure className="w-full w-36 w-48 md:w-72 lg:w-96 h-auto flex flex-nowrap items-center text-lg sm:text-2xl lg:text-3xl font-semibold">
-          <img alt="" src={logo} className="w-6 sm:w-8 md:w-12"/>
+          <img alt="" src={logo} width="48" height="48" className="w-6 sm:w-8 md:w-12 h-auto"/>
           <figcaption className="uppercase text-white ml-4 lg:ml-12">SCC Uplifting</figcaption>
         </figure>
         <nav className="hidden md:flex flex-nowrap space-x-2 md:space-x-4 lg:space-x-8 text-gold">
           <a href="#mission" className={naStyle}>About</a>
           <a href="#portfolio" className={naStyle}>Portfolio</a>
-          <a href="#case_studies" className={naStyle}>Case Studies</a>
-          <a href="#footer" className={naStyle}>Contact</a>
+          <a href="#process" className={naStyle}>Process</a>
+          <a href="#case_studies" className={naStyle+' hidden'}>Case Studies</a>
+          <a href="mailto:contact@sccupliftinginc.com" className={naStyle}>Contact</a>
         </nav>
       </header>
       <article className={rowStyle + " " + conStyle + " pt-24 md:min-h-screen relative items-center bg-black text-white bg-cover bg-fixed bg-bottom bg-right"}
@@ -132,7 +133,7 @@ function App() {
       }}>
         <div className="w-full p-8 sm:px-16 sm:py-24 md:p-0">
           <h1 className="mb-4 md:mb-8 text-3xl md:text-4xl lg:text-5xl mb-2 md:mb-4 font-extrabold drop-shadow-md">Infrastructure Creates Opportunity</h1>
-          <hr className="w-12 lg:w-44 h-1 bg-teal-hero ml-0 mb-4 md:mb-8"/>
+          <hr className="w-12 lg:w-44 h-1 bg-periwinkle ml-0 mb-4 md:mb-8"/>
           <p className="w-full md:w-2/3 drop-shadow-sm text-xl md:text-2xl">We believe in improving services and creating new streams of revenue to sustain our cities</p>
         </div>
       </article>
@@ -141,23 +142,23 @@ function App() {
           { makeEyebrow('Our Portfolio')}
           { makeHeading('Our Recent Work')}
           <hr className="w-12 lg:w-28 bg-teal md:-mt-3 mb-2 md:mb-5"/>
-          <p className="text-neutral-400 text-sm md:text-base">Sed&nbsp;finibus rutrum leo vitae congue. Sed nec erat convallis, convallis nulla a, placerat ipsum. Nunc posuere leo non justo lacinia, sed feugiat nisi commodo. Proin vulputate risus vitae quam viverra vestibulum ut a lacus.</p>
+          <p className="text-neutral-400 text-sm md:text-base hidden">Sed&nbsp;finibus rutrum leo vitae congue. Sed nec erat convallis, convallis nulla a, placerat ipsum. Nunc posuere leo non justo lacinia, sed feugiat nisi commodo. Proin vulputate risus vitae quam viverra vestibulum ut a lacus.</p>
         </div>
         <ol className="grid sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 min-w-full md:-mx-4">
           <li data-slide className={slideStyle}>
             { makeSlide(redBuilding,
-              "Dennis Brew Fire Station"
-            )}
+              "Dennis Brew Fire Station", 
+              'rgb(94 0 75)', 'rgb(160 122 145/ 0.4)','#a07a91','',640,427)}
           </li>
           <li data-slide className={slideStyle}>
             { makeSlide(couches, 
               "Project #2 Unnamed",
-              '#26443E', 'rgba(27, 42, 73, 0.58)','hsla(177, 26%, 55%, 1)')}
+              undefined, undefined,undefined,'',640,427)}
           </li>
           <li data-slide className={slideStyle + " block sm:hidden md:block"}>
             { makeSlide(entryway, 
               "Project #3 Unnamed",
-              '#203837', 'rgba(44, 28, 28, 0.45)','hsla(177, 26%, 55%, 1)')}
+              '#203837', 'rgba(44, 28, 28, 0.45)','hsla(177, 26%, 55%, 1)','',640,427)}
           </li>
         </ol>
         <div id="mission" className="flex flex-wrap md:flex-nowrap justify-between order-2 pt-8 md:pt-16 gap-4 md:gap-24">
@@ -165,15 +166,15 @@ function App() {
             { makeSubheading('Improving Underserved Communities')}
             <p className="text-neutral-400 text-sm md:text-base">With over 20 years of experience in construction and development, we are one team, driven to uplift the community and make it better.</p>
           </div>
-          <dl data-cross className="relative w-full px-2 md:w-3/5 grid sm:grid-cols-2 text-white">
+          <ul data-cross className="relative w-full px-2 md:w-3/5 grid sm:grid-cols-2 text-white">
             { makeCross('Professional services spanning cities','#442920','#9C7D76') }
             { makeCross('Performance throughout the project lifespan') }
-            { makeCross('Understanding the challenges our communities face','#3B3325','#7D6D51') }
+            { makeCross('Understanding the challenges our communities face','#5e0039','#a07a91') }
             { makeCross('Working to connect funding and provide guidance','#1C2B4A','#5072B9') }
-          </dl>
+          </ul>
         </div>
       </section>
-      <section id="case_studies" className={rowStyle + '  md:flex-nowrap min-h-screen bg-chocolate'}>
+      <section id="case_studies" className={rowStyle + '  md:flex-nowrap min-h-screen bg-chocolate hidden'}>
         <div className="w-full md:w-1/2 order-2 px-4 pt-8 pb-0 md:p-16 bg-chocolate-gradient">
           { makeSubheading('This is Where We Put Our Case Study Which Talks About the Project')}
           <p className="text-neutral-400 text-sm md:text-base">Cras in felis eu nunc efficitur bibendum. Praesent tortor purus, efficitur non ultricies sed, accumsan in elit. Suspendisse rhoncus mauris at turpis interdum, in varius arcu tempor.</p>
@@ -189,33 +190,30 @@ function App() {
            style={{
             backgroundImage: `url(${redWalls})`
           }}>
-            <img alt="" src={redWalls} className="w-full h-auto invisible"/>
+            <img width="480" height="320" alt="" src={redWalls} className="invisible"/>
           </div>
         </picture>
       </section>
-      <section id="process" className={rowStyle + " " + conStyle + " text-center items-center justify-center"}>
-        <div className="w-full text-white text-center">
-          { makeHeading('This is How We Work')}
+      <aside id="process" className="flex flex-wrap md:flex-nowrap w-full h-auto py-8 md:py-16 px-4 md:px-12 xl:px-24 align-center justify-center py-24 bg-cover bg-center bg-fixed" 
+      style={{
+        backgroundImage: `url(${office})`
+      }}>
+        <div className="outline outline-4 md:outline-8 lg:outline-16 outline-eggplant-1/3 bg-eggplant rounded-lg md:rounded-xl lg:rounded-2xl p-8 lg:p-16 text-center">
+          <h3 className="mb-2 md:mb-4 text-3xl md:text-4xl lg:text-6xl font-extrabold text-white">This is How We Work</h3>
+          <dl data-touts className="w-full grid md:grid-cols-3 gap-x-24 -mt-8 md:mt-auto text-white">
+            { makeTout('We are accessible',"Team Members are available to speak about goals for your city")}
+            { makeTout('We offer flexible plans',"Make your first payment two years after project completion")}
+            { makeTout('We work with you',"Improving first and second year revenue, to make repayment easy!")}
+          </dl>
         </div>
-        <dl data-touts className="w-full grid md:grid-cols-3 gap-x-24 -mt-8 md:mt-auto">
-          { makeTout('We are accessible',"Team Members are available to speak about goals for your city")}
-          { makeTout('We offer flexible plans',"Make your first payment two years after project completion")}
-          { makeTout('We work with you',"Improving first and second year revenue, to make repayment easy!")}
-        </dl>
-      </section>
+      </aside>
       <footer id="footer" className={rowStyle + " " + conStyle + " flex-wrap bg-chocolate py-4 md:py-8"}>
         <div className="flex flex-wrap md:flex-nowrap items-center justify-between w-full">
           <figure className="w-24 md:w-48 h-auto text-gold-1/2 mx-auto mb-4 md:ml-0 md:mb-auto">
-            <img alt="" src={logo_stacked}/>
+            <img alt="SCC UPLIFTING" width="86" height="117" src={logo_stacked}/>
           </figure>  
           <div className="w-full md:w-3/4">
-            <nav className="w-full flex justify-center md:justify-start items-center space-x-2 md:space-x-4 lg:space-x-28 mb-4 md:mb-8 text-gold">
-              <a href="#mission" className={fLinkStyle}>About </a>
-              <a href="#case_studies" className={fLinkStyle}>Case Studies</a>
-              <a href="#portfolio" className={fLinkStyle}>Portfolio</a>
-              <a href="mailto:contact@sccupliftinginc.com" className={fLinkStyle}>Contact</a>
-            </nav>
-            <dl className="grid grid-cols-3 justify-between w-full text-white text-xs md:text-sm">
+            <dl className="grid grid-cols-1 md:grid-cols-3 md:justify-between w-full text-center md:text-left text-white text-xs md:text-sm">
               <dt className={fDtStyle}>Phone</dt>
               <dd className={fDdStyle}>
                 <a href="tel:+18001234567890">[800] 123.456.7890</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,11 @@ module.exports = {
           '1/3': 'rgb(0 0 0 / .33)',
           '2/3': 'rgb(0 0 0 / .66)'
         },
+        'eggplant': {
+          'DEFAULT':'#5e0039',
+          '1/2': 'rgb(94 0 57 / .5)',
+          '1/3': 'rgb(94 0 57 / .33)'
+        },
         'gold': {
           'DEFAULT':'#C1A45C',
           '1/2': 'rgb(181 170 72 / .5)',
@@ -24,6 +29,9 @@ module.exports = {
           'DEFAULT':'#412B23',
           '300': 'hsla(8, 27%, 59%, 1)',
           '100': '#BDB69F'
+        },
+        'periwinkle': {
+          'DEFAULT':'rgb(94 0 75)'
         },
         'neutral': {
           '850': '#1a1a1a',
